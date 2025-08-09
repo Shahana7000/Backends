7a032dc46e0041e5a3b8f4cb7b531efa45e088fb
Shahana700 <shahanacs2003@gmail.com>
2025-08-09 20:16:41 +0530

    node with database

--- SQL_DATABASE/index.js
+++ SQL_DATABASE/index.js
@@ -1,5 +1,14 @@
 const { faker } = require('@faker-js/faker');
 const mysql = require('mysql2');
+const express = require('express');
+const app = express();
+const path = require('path'); 
+const methodOverride = require('method-override');
+
+app.use(methodOverride("_method"));
+app.use(express.urlencoded({extended : true}));
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "/views"));
 
 const connection = mysql.createConnection({
     host: 'localhost',
@@ -16,21 +25,70 @@ let getRandomUser = () => {
     ];
 };
 
-let q = "INSERT INTO `users` (`id`, `name`, `email`, `password`) VALUES ?";
-let data = [];
+app.get("/", (req, res) => {
+    let q = `SELECT count(*) FROM users`;
+    try {
+        connection.query(q, (err, result) => {
+            if (err) throw err;
+            let count = result[0]['count(*)'];
+            console.log(count);
+            res.render("home.ejs", { count });
+        });
+    } catch (error) {
+        console.log("Error in the query:", error);
+    }
+});
 
-for(let i = 1;i<=100;i++){
-    data.push(getRandomUser());
-}
+app.get("/users", (req, res) => {
+    let q = `SELECT * FROM users`;
+    try {
+        connection.query(q, (err, users) => {
+            if (err) throw err;
+            res.render("showusers.ejs",{users});
+        });
+    } catch (error) {
+        console.log("Error in the query:", error);
+    }
+});
 
+app.get("/users/:id/edit", (req,res) =>{
+    let {id} = req.params;
+    let q = `SELECT * FROM users WHERE id = '${id}'`;
+    try {
+        connection.query(q, (err, result) => {
+            if (err) throw err;
+            let user = result[0];
+            res.render("edit.ejs", { user }); // pass the user to the template
+        });
+    } catch (error) {
+        console.log("Error in the query:", error);
+    }
+});
 
-try {
-    connection.query(q, [data], (err, result) => {
-        if (err) throw err;
-        console.log(result);
-    });
-} catch (error) {
-    console.log("Error:", error);
-}
+app.patch("/users/:id", (req,res) =>{
+    let {id} = req.params;
+    let {password : formPass, username : newUsername} = req.body;
+    let q = `SELECT * FROM users WHERE id = '${id}'`;
+    try {
+        connection.query(q, (err, result) => {
+            if (err) throw err;
+           let user = result[0];
+           if(formPass != user.password){
+            res.send("wrong password");
+           }else{
+            let q2 = `UPDATE users SET name = '${newUsername}' WHERE id = '${id}'`;
+            connection.query(q2, (err, result) => {
+                if (err) throw err;
+                res.redirect("/users");
+           });
+        }
 
-connection.end();
+        });
+    
+    } catch (error) {
+        console.log("Error in the query:", error);
+    }
+});
+app.listen(8080, () =>{
+    console.log("Server is running on port 8080");
+});
