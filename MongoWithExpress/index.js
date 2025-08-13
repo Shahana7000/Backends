97ce7ea8f3e8b35abee0430dec91ffb520c0a466
Shahana700 <shahanacs2003@gmail.com>
2025-08-13 16:58:10 +0530

    crud operation

--- MongoWithExpress/index.js
+++ MongoWithExpress/index.js
@@ -3,12 +3,15 @@ const app = express();
 const mongoose = require('mongoose');
 const path = require('path');
 const Chat = require('./models/chat.js');
+const methodOverride = require('method-override');
 
 
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.static(path.join(__dirname, "public")));
+app.use(express.urlencoded({extended : true}))
+app.use(methodOverride('_method'));
 
 main().
 then(() => {
@@ -25,13 +28,53 @@ async function main() {
 
 app.get('/chats',async(req, res) => {
        let chats = await Chat.find()
-       console.log(chats)
+    //    console.log(chats)
        res.render("index.ejs",{chats})
        
 })
 
 app.get("/chats/new", (req,res) =>{
     res.render("new.ejs")
+})
+
+app.post("/chats", (req,res) =>{
+    let {from, to, msg} = req.body;
+    let newChat = new Chat({
+        from : from,
+        to : to,
+        msg : msg,
+        created_at : new Date()
+    })
+    newChat.save().then(res => {
+        console.log("chat was saved")
+    })
+    .catch(err => {
+        console.log(err)
+})
+    res.redirect("/chats");
+})
+
+app.get("/chats/:id/edit", async(req,res) =>{
+    let id = req.params.id;
+    let chat = await Chat.findById(id);
+    res.render("edit.ejs", {chat});
+})
+
+app.put("/chats/:id", async(req,res) =>{
+    let id = req.params.id;
+    let {msg : newMsg} = req.body;
+    let updatedchat = await Chat.findByIdAndUpdate(id, {msg : newMsg}, {runValidators : true, new : true}
+
+    );
+    console.log(updatedchat)
+    res.redirect("/chats");
+})
+
+app.delete("/chats/:id", async(req,res) =>{
+    let id = req.params.id;
+   let deletedchat =  await Chat.findByIdAndDelete(id);
+   console.log(deletedchat)
+   res.redirect("/chats");
 })
 
 app.get("/", (req,res) =>{
